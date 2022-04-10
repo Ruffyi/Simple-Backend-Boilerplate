764de7ef6480c8c0ce388688e3de5eb55887e768
Kamil <kam.nas21@wp.pl>
2022-04-10 19:48:59 +0200

    #1

--- server.ts
+++ server.ts
@@ -1,10 +1,13 @@
 import express, { Application, Request, Response } from 'express';
+import cors from 'cors';
 
 import dotenv from 'dotenv';
 
 dotenv.config({ path: './config/config.env' });
 
 const app: Application = express();
+
+app.use(cors());
 
 const SERVER_PORT = process.env.SERVER_PORT || 5000;
 
