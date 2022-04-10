876cbba0f4e69345e746d7ae5b7347f48d614f60
Kamil <kam.nas21@wp.pl>
2022-04-10 21:52:34 +0200

    #122

--- server.ts
+++ server.ts
@@ -4,6 +4,8 @@ import userRouter from './routes/userRouter';
 
 import { allowCors, handler } from './utils/cors';
 
+import cors from 'cors';
+
 import dotenv from 'dotenv';
 
 import connectToMongoDB from './db/connectDB';
@@ -14,11 +16,13 @@ connectToMongoDB();
 
 const app: Application = express();
 
+app.use(cors());
+
 const SERVER_PORT = process.env.SERVER_PORT || 5000;
 
 app.get('/', userRouter);
 
-app.use(allowCors(handler));
+// app.use(allowCors(handler));
 
 app.listen(SERVER_PORT, () => {
 	console.log(`Server is listening at port : ${SERVER_PORT}`);
